8372204128451fc6d71a811ac8e3ef6217a1d8be
matamorphosis <mbrittain15@gmail.com>
2019-07-12 12:06:40 +1000

    Add files via upload

---
+++ bin/plugins/Library_Genesis_Search.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests, re, plugins.common.General as General
+
+Plugin_Name = "Library-Genesis"
+Concat_Plugin_Name = "libgen"
+The_File_Extension = ".html"
+
+def Search(Query_List, Task_ID, **kwargs):
+    Data_to_Cache = []
+    Cached_Data = []
+
+    if "Limit" in kwargs:
+
+        if int(kwargs["Limit"]) > 0:
+            Limit = kwargs["Limit"]
+
+    else:
+        Limit = 10
+
+    Directory = General.Make_Directory(Concat_Plugin_Name)
+    General.Logging(Directory, Concat_Plugin_Name)
+    Cached_Data = General.Get_Cache(Directory, Plugin_Name)
+
+    if not Cached_Data:
+        Cached_Data = []
+
+    Query_List = General.Convert_to_List(Query_List)
+
+    for Query in Query_List:
+        # Query can be Title or ISBN
+        Main_URL = "http://gen.lib.rus.ec/search.php?req=" + Query + "&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
+        Lib_Gen_Response = requests.get(Main_URL).text
+        General.Main_File_Create(Directory, Plugin_Name, Lib_Gen_Response, Query, The_File_Extension)
+        Lib_Gen_Regex = re.findall("book\/index\.php\?md5=[A-Fa-f0-9]{32}", Lib_Gen_Response)
+
+        if Lib_Gen_Regex:
+            Current_Step = 0
+
+            for Regex in Lib_Gen_Regex:
+                Item_URL = "http://gen.lib.rus.ec/" + Regex
+                Lib_Item_Response = requests.get(Item_URL).text
+
+                if Item_URL not in Cached_Data and Item_URL not in Data_to_Cache and Current_Step < int(Limit):
+                    Output_file = General.Create_Query_Results_Output_File(Directory, Query, Plugin_Name, Lib_Item_Response, Regex, The_File_Extension)
+
+                    if Output_file:
+                        General.Connections(Output_file, Query, Plugin_Name, Item_URL, "gen.lib.rus.ec", "Data Leakage", Task_ID, General.Get_Title(Item_URL), Concat_Plugin_Name)
+
+                    Data_to_Cache.append(Item_URL)
+                    Current_Step += 1
+
+    if Cached_Data:
+        General.Write_Cache(Directory, Data_to_Cache, Plugin_Name, "a")
+
+    else:
+        General.Write_Cache(Directory, Data_to_Cache, Plugin_Name, "w")
