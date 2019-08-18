586cc1efd0c6ddbe4f2f2447e9b59572aa30d265
matamorphosis <mbrittain15@gmail.com>
2019-08-18 14:07:17 +1000

    Update dependencies.sh

--- installation/dependencies.sh
+++ installation/dependencies.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -f /etc/lsb-release ]; then
 	apt update
-	apt install -y python3 python3-pip python3-psycopg2 postgresql postgresql-contrib ruby rubygems build-essential wget unzip git
+	apt install -y python3 python3-pip python3-psycopg2 python3-werkzeug postgresql postgresql-contrib ruby rubygems build-essential wget unzip git
 fi
 
 if [ -e /etc/os-release ]; then
