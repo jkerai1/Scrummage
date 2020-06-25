8396e7957d8172872021adf666e4bfaecd886e18
matamorphosis <mbrittain15@gmail.com>
2020-06-25 20:48:46 +1000

    Update Generate_JSON_Config.py

--- installation/Generate_JSON_Config.py
+++ installation/Generate_JSON_Config.py
@@ -22,181 +22,137 @@ if Arguments.database and Arguments.username and Arguments.password:
         Open_File = open(Configuration_File,"w+")
 
         JSON_Payload = '''{
-            "craigslist": [
-                {
-                    "city": "Sydney"
-                }
-            ],
-            "csv": [
-                {
-                    "use-csv": false
-                }
-            ],
-            "docx-report": [
-                {
-                    "use-docx": false
-                }
-            ],
-            "defectdojo": [
-                {
-                    "api_key": "",
-                    "host": "https://host.com",
-                    "user": "admin",
-                    "engagement-id": 1,
-                    "product-id": 1,
-                    "test-id": 1,
-                    "user-id": 1
-                }
-            ],
-            "ebay": [
-                {
-                    "access_key": ""
-                }
-            ],
-            "email": [
-                {
-                    "smtp_server": "",
-                    "smtp_port": 25,
-                    "from_address": "",
-                    "from_password": "",
-                    "to_address": ""
-                }
-            ],
-            "flickr": [
-                {
-                    "api_key": "",
-                    "api_secret": ""
-                }
-            ],
-            "elasticsearch": [
-                {
-                    "service": "http://",
-                    "host": "",
-                    "port": 9200
-                }
-            ],
-            "general": [
-                {
-                    "location": "au"
-                }
-            ],
-            "google": [
-                {
-                    "cx": "",
-                    "application_name": "",
-                    "application_version": "v1",
-                    "developer_key": ""
-                }
-            ],
-            "haveibeenpwned": [
-                {
-                    "api_key": ""
-                }
-            ],
-            "JIRA": [
-                {
-                    "project_key": "",
-                    "address": "",
-                    "username": "",
-                    "password": "",
-                    "ticket_type": ""
-                }
-            ],
-            "pinterest": [
-                {
-                    "oauth_token": ""
-                }
-            ],
-            "postgresql": [
-                {
-                    "host": "127.0.0.1",
-                    "port": 5432,
-                    "database": "''' + Arguments.database + '''",
-                    "user": "''' + Arguments.username + '''",
-                    "password": "''' + Arguments.password + '''"
-                }
-            ],
-            "reddit": [
-                {
-                    "client_id": "",
-                    "client_secret": "",
-                    "user_agent": "",
-                    "username": "",
-                    "password": "",
-                    "subreddits": "all"
-                }
-            ],
-            "rtir": [
-                {
-                    "service": "http",
-                    "host": "",
-                    "port": 80,
-                    "user": "",
-                    "password": "",
-                    "authenticator": ""
-                }
-            ],
-            "scumblr": [
-                {
-                    "host": "",
-                    "port": 5432,
-                    "database": "",
-                    "user": "",
-                    "password": ""
-                }
-            ],
-            "slack": [
-                {
-                    "token": "",
-                    "channel": ""
-                }
-            ],
-            "sslmate": [
-                {
-                    "api_key": "",
-                    "search_subdomain": "true or false"
-                }
-            ],
-            "twitter": [
-                {
-                    "CONSUMER_KEY": "",
-                    "CONSUMER_SECRET": "",
-                    "ACCESS_KEY": "",
-                    "ACCESS_SECRET": ""
-                }
-            ],
-            "ukbusiness": [
-                {
-                    "api_key": ""
-                }
-            ],
-            "vulners": [
-                {
-                    "api_key": ""
-                }
-            ],
-            "web-app": [
-                {
-                    "debug": false,
-                    "host": "127.0.0.1",
-                    "port": 5000,
-                    "certificate-file": "../certs/certificate.crt",
-                    "key-file": "../certs/privateKey.key",
-                    "api-secret": "''' + API_Secret + '''",
-                    "api-validity-minutes": 1440,
-                    "api-max-calls": 10,
-                    "api-period-in-seconds": 60
-                }
-            ],
-            "youtube": [
-                {
-                    "developer_key": "",
-                    "application_name": "",
-                    "application_version": "v3",
-                    "location": "37.42307,-122.08427",
-                    "location_radius": "5km"
-                }
-            ]
+            "craigslist": {
+                "city": "Sydney"
+            },
+            "csv": {
+                "use-csv": false
+            },
+            "docx-report": {
+                "use-docx": false
+            },
+            "defectdojo": {
+                "api_key": "",
+                "host": "https://host.com",
+                "user": "admin",
+                "engagement-id": 1,
+                "product-id": 1,
+                "test-id": 1,
+                "user-id": 1
+            },
+            "ebay": {
+                "access_key": ""
+            },
+            "email": {
+                "smtp_server": "",
+                "smtp_port": 25,
+                "from_address": "",
+                "from_password": "",
+                "to_address": ""
+            },
+            "flickr": {
+                "api_key": "",
+                "api_secret": ""
+            },
+            "elasticsearch": {
+                "service": "http://",
+                "host": "",
+                "port": 9200
+            },
+            "general": {
+                "location": "au"
+            },
+            "google": {
+                "cx": "",
+                "application_name": "",
+                "application_version": "v1",
+                "developer_key": ""
+            },
+            "google-chrome": {
+                "application-path": "/usr/bin/google-chrome",
+                "chromedriver-path": "/usr/bin/chromedriver"
+            },
+            "haveibeenpwned": {
+                "api_key": ""
+            },
+            "JIRA": {
+                "project_key": "",
+                "address": "",
+                "username": "",
+                "password": "",
+                "ticket_type": ""
+            },
+            "pinterest": {
+                "oauth_token": ""
+            },
+            "postgresql": {
+                "host": "127.0.0.1",
+                "port": 5432,
+                "database": "''' + Arguments.database + '''",
+                "user": "''' + Arguments.username + '''",
+                "password": "''' + Arguments.password + '''"
+            },
+            "reddit": {
+                "client_id": "",
+                "client_secret": "",
+                "user_agent": "",
+                "username": "",
+                "password": "",
+                "subreddits": "all"
+            },
+            "rtir": {
+                "service": "http",
+                "host": "",
+                "port": 80,
+                "user": "",
+                "password": "",
+                "authenticator": ""
+            },
+            "scumblr": {
+                "host": "",
+                "port": 5432,
+                "database": "",
+                "user": "",
+                "password": ""
+            },
+            "slack": {
+                "token": "",
+                "channel": ""
+            },
+            "sslmate": {
+                "api_key": "",
+                "search_subdomain": "true or false"
+            },
+            "twitter": {
+                "CONSUMER_KEY": "",
+                "CONSUMER_SECRET": "",
+                "ACCESS_KEY": "",
+                "ACCESS_SECRET": ""
+            },
+            "ukbusiness": {
+                "api_key": ""
+            },
+            "vulners": {
+                "api_key": ""
+            },
+            "web-app": {
+                "debug": false,
+                "host": "127.0.0.1",
+                "port": 5000,
+                "certificate-file": "../certs/certificate.crt",
+                "key-file": "../certs/privateKey.key",
+                "api-secret": "''' + API_Secret + '''",
+                "api-validity-minutes": 1440,
+                "api-max-calls": 10,
+                "api-period-in-seconds": 60
+            },
+            "youtube": {
+                "developer_key": "",
+                "application_name": "",
+                "application_version": "v3",
+                "location": "37.42307,-122.08427",
+                "location_radius": "5km"
+            }
         }'''
 
         Open_File.write(JSON_Payload)
