495a4e51e6edc50c24285b7087883340d400fa4a
matamorphosis <mbrittain15@gmail.com>
2020-07-05 19:50:23 +1000

    modified:   Google_Search.py

--- lib/plugins/Google_Search.py
+++ lib/plugins/Google_Search.py
@@ -48,43 +48,47 @@ def Search(Query_List, Task_ID, **kwargs):
         Limit = General.Get_Limit(kwargs)
 
         for Query in Query_List:
-            Service = build("customsearch", Google_Details[3], developerKey=Google_Details[1])
+            Service = build("customsearch", Google_Details[2], developerKey=Google_Details[3], cache_discovery=False)
             CSE_Response = Service.cse().list(q=Query, cx=Google_Details[0], num=Limit).execute()
             CSE_JSON_Output_Response = json.dumps(CSE_Response, indent=4, sort_keys=True)
             CSE_JSON_Response = json.loads(CSE_JSON_Output_Response)
-
             Main_File = General.Main_File_Create(Directory, Plugin_Name, CSE_JSON_Output_Response, Query, The_File_Extensions["Main"])
             Output_Connections = General.Connections(Query, Plugin_Name, "google.com", "Domain Spoof", Task_ID, Plugin_Name.lower())
 
-            for JSON_Response_Items in CSE_JSON_Response['items']:
+            if 'items' in CSE_JSON_Response:
 
-                try:
-                    Google_Item = JSON_Response_Items['pagemap']['metatags']
+                for JSON_Response_Items in CSE_JSON_Response['items']:
 
-                    for Google_Item_Line in Google_Item:
-                        Google_Item_URL = Google_Item_Line['og:url']
+                    try:
+                        Google_Item = JSON_Response_Items['pagemap']['metatags']
 
-                        if Google_Item_URL not in Cached_Data and Google_Item_URL not in Data_to_Cache:
-                            Path_Regex = re.search(r"https?\:\/\/(www\.)?[\w\d\.]+\.\w{2,3}(\.\w{2,3})?(\.\w{2,3})?\/([\w\d\-\_\/]+)?", Google_Item_URL)
+                        for Google_Item_Line in Google_Item:
+                            Google_Item_URL = Google_Item_Line['og:url']
 
-                            if Path_Regex:
-                                headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0', 'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5'}
-                                Google_Item_Response = requests.get(Google_Item_URL, headers=headers).text
-                                Output_Path = Path_Regex.group(4).replace("/", "-")
-                                Output_file = General.Create_Query_Results_Output_File(Directory, Query, Plugin_Name, Google_Item_Response, Output_Path, The_File_Extensions["Query"])
+                            if Google_Item_URL not in Cached_Data and Google_Item_URL not in Data_to_Cache:
+                                Path_Regex = re.search(r"https?\:\/\/(www\.)?[\w\d\.]+\.\w{2,3}(\.\w{2,3})?(\.\w{2,3})?\/([\w\d\-\_\/]+)?", Google_Item_URL)
 
-                                if Output_file:
-                                    Output_Connections.Output([Main_File, Output_file], Google_Item_URL, General.Get_Title(Google_Item_URL), Plugin_Name.lower())
-                                    Data_to_Cache.append(Google_Item_URL)
+                                if Path_Regex:
+                                    headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0', 'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5'}
+                                    Google_Item_Response = requests.get(Google_Item_URL, headers=headers).text
+                                    Output_Path = Path_Regex.group(4).replace("/", "-")
+                                    Output_file = General.Create_Query_Results_Output_File(Directory, Query, Plugin_Name, Google_Item_Response, Output_Path, The_File_Extensions["Query"])
+
+                                    if Output_file:
+                                        Output_Connections.Output([Main_File, Output_file], Google_Item_URL, General.Get_Title(Google_Item_URL), Plugin_Name.lower())
+                                        Data_to_Cache.append(Google_Item_URL)
+
+                                    else:
+                                        logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - Failed to create output file. File may already exist.")
 
                                 else:
-                                    logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - Failed to create output file. File may already exist.")
+                                    logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - Failed to match regular expression.")
 
-                            else:
-                                logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - Failed to match regular expression.")
+                    except Exception as e:
+                        logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - {str(e)}")
 
-                except Exception as e:
-                    logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - {str(e)}")
+            else:
+                logging.warning(f"{General.Date()} - {__name__.strip('plugins.')} - No results found.")
 
         if Cached_Data:
             General.Write_Cache(Directory, Data_to_Cache, Plugin_Name, "a")
